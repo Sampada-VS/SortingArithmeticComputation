556fe9f847aaff5f5102d00a9e8b6b8a521efcf8
Sampada-VS <samssvv1@gmail.com>
2020-08-05 23:42:38 +0530

    Adding-Compute a%b+c

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -3,3 +3,4 @@ read -p "Enter three numbers :" a b c
 compute1=`awk "BEGIN{ print $a+$b*$c }"`
 compute2=`awk "BEGIN{ print $a*$b+$c }"`
 compute3=`awk "BEGIN{ print $c+$a/$b }"`
+compute4=`awk "BEGIN{ print $a%$b+$c }"`
