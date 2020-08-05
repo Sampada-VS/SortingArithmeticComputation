bbb62b187bdd8412f4cf377c559daa1ba61006c8
Sampada-VS <samssvv1@gmail.com>
2020-08-06 00:22:38 +0530

    Storing computation result from dictionary into array

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -1,28 +1,14 @@
 #!/bin/bash -x
 read -p "Enter three numbers :" a b c
 declare -A computeDict
-
-computeDict["a+b*c"]=`awk "BEGIN{ print $a+$b*$c }"`
-computeDict["a*b+c"]=`awk "BEGIN{ print $a*$b+$c }"`
-computeDict["c+a/b"]=`awk "BEGIN{ print $c+$a/$b }"`
-computeDict["a%b+c"]=`awk "BEGIN{ print $a%$b+$c }"`
+computeDict[1]=`awk "BEGIN{ print $a+$b*$c }"`
+computeDict[2]=`awk "BEGIN{ print $a*$b+$c }"`
+computeDict[3]=`awk "BEGIN{ print $c+$a/$b }"`
+computeDict[4]=`awk "BEGIN{ print $a%$b+$c }"`
 echo "Computation Dictionary :" ${computeDict[@]}
 
 for (( i=1; i<=4; i++ ))
 do
-	computeArray[$i]=${computeDict[$i]}
+        computeArray[$i]=${computeDict[$i]}
 done
 echo "Computation Array :" ${computeArray[@]}
-for (( i=1; i<=4; i++ ))
-do
-	for (( j=$(($i+1)); j<=4; j++ ))
-	do
-                if (( $(awk 'BEGIN {print ('${computeArray[$i]}' > '${computeArray[$j]}')}') ))
-		then
-			temp=${computeArray[$i]}
-			computeArray[$i]=${computeArray[$j]}
-			computeArray[$j]=$temp
-		fi
-	done
-done
-echo "Result in Descending order :" ${computeArray[@]}
