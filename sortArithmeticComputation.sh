39414edf5c79190bb5895f3a171d76ed6d31b59e
Sampada-VS <samssvv1@gmail.com>
2020-08-06 00:27:44 +0530

    Sorting computation result array in descending order

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -8,7 +8,19 @@ computeDict[4]=`awk "BEGIN{ print $a%$b+$c }"`
 echo "Computation Dictionary :" ${computeDict[@]}
 
 for (( i=1; i<=4; i++ ))
-do
-        computeArray[$i]=${computeDict[$i]}
+	computeArray[$i]=${computeDict[$i]}
 done
 echo "Computation Array :" ${computeArray[@]}
+for (( i=1; i<=4; i++ ))
+do
+	for (( j=$(($i+1)); j<=4; j++ ))
+	do
+		if (( $(awk 'BEGIN {print ('${computeArray[$i]}' < '${computeArray[$j]}')}') ))
+		then
+			temp=${computeArray[$i]}
+			computeArray[$i]=${computeArray[$j]}
+			computeArray[$j]=$temp
+		fi
+	done
+done
+echo "Result in Descending order :" ${computeArray[@]}
